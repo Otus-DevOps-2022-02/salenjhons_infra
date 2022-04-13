11766dbb6b51e79a0a9a067f0dba22a7a889680a
salenjhons <electromehanic@rambler.ru>
2022-04-13 09:34:25 +0300

    Fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_providers {
+    yandex = "~> 0.35"
+  }
+  required_version = "~> 0.12.0"
+}
+
 provider "yandex" {
   version = "~> 0.35"
   service_account_key_file = var.service_account_key_file
