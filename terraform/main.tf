9a9d3828d68a070389f482b140e391cf6497f207
salenjhons <electromehanic@rambler.ru>
2022-04-13 09:18:38 +0300

    6th fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-      version = "0.73.0"
-    }
-  }
-}
-
 provider "yandex" {
+  version = "~> 0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
