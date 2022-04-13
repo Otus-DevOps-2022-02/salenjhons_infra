bfe657b750bf9736e457582639336058a31f8748
salenjhons <electromehanic@rambler.ru>
2022-04-13 08:42:57 +0300

    5th fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,18 +16,36 @@ provider "yandex" {
 
 resource "yandex_compute_instance" "app" {
   count = var.instance_count
-  name  = var.instance_name[count_index]
-
+  name  = "reddit-app-${count.index}"
+  zone = var.zone
   resources {
     cores         = 2
-    memory        = 4
+    memory        = 2
     core_fraction = 5
   }
+
 
   boot_disk {
     initialize_params {
       image_id = var.image_id
     }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = yandex_compute_instance.app.0.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file(var.private_key_path)
   }
 
   provisioner "file" {
@@ -38,23 +56,4 @@ resource "yandex_compute_instance" "app" {
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
-
-  connection {
-    type        = "ssh"
-    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
-    private_key = file(var.private_key_path)
-    timeout     = "1m"
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  network_interface {
-    subnet_id = var.subnet_id
-    nat       = true
-  }
-
 }
