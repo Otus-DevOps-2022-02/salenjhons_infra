307e80edeb15427edde95ca0bee10e31abf318b3
salenjhons <electromehanic@rambler.ru>
2022-04-13 07:40:32 +0300

    2nd fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "yandex" {
-  version                  = "~>0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
